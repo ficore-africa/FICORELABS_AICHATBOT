eda6ce1e2cabee47639b1547662308e7a12f2a37
ficore-africa <ficoreafrica@gmail.com>
2025-07-19 09:37:08 +0100

    Update routes.py

--- ficore-accounting/creditors/routes.py
+++ ficore-accounting/creditors/routes.py
@@ -10,7 +10,7 @@ import io
 import re
 import urllib.parse
 import utils
-from translations import trans, register_translation
+from translations import trans
 
 logger = logging.getLogger(__name__)
 
